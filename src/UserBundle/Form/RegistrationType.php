c4083bc0c10e2453077d339f5d53907ec712265d
kyller92 <kyller92@hotmail.fr>
2017-09-25 17:17:43 +0200

    Form modification

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -5,18 +5,27 @@ namespace UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class RegistrationType extends AbstractType{
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('address', TextType::class);
+        $builder
+        ->add('firstName', TextType::class)
+        ->add('lastName', TextType::class)
+        ->add('address', TextType::class)
+        ->add('dateBirth', DateType::class)
+        ->add('phoneNumber', TextType::class)
+        ->add('drivingLicence', TextType::class)
+        
+        ;
     }
 
     public function getBlockPrefix()
     {
         return 'app_user_registration';
     }
-    
+
     public function getParent()
    	{
       return 'FOS\UserBundle\Form\Type\RegistrationFormType';

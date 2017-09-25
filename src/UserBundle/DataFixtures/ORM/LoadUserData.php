09bbf231d87371f4bdb593e2595f1b83981efc7e
Simon Turquier <turquiersimon@hotmail.fr>
2017-09-25 15:15:41 +0200

    Add User fixture

---
+++ src/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace UserBundle\DataFixtures\ORM;
+
+use UserBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $user1 = new User();
+        $user1->setFirstName('simon');
+        $user1->setLastName('turquier');
+        $user1->setPlainPassword('sturquier');
+        $user1->setDateBirth($faker->dateTime($max = 'now'));
+        $user1->setAddress($faker->address);
+        $user1->setPhoneNumber($faker->phoneNumber);
+        $user1->setDrivingLicense($faker->bothify('##??#####'));
+        $user1->setEmail('turquiersimon@hotmail.fr');
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setFirstName('sophie');
+        $user2->setLastName('lee');
+        $user2->setPlainPassword('stitchMoonz');
+        $user2->setDateBirth($faker->dateTime($max = 'now'));
+        $user2->setAddress($faker->address);
+        $user2->setPhoneNumber($faker->phoneNumber);
+        $user2->setDrivingLicense($faker->bothify('##??#####'));
+        $user2->setEmail('lee.sophie0@gmail.com');
+        $manager->persist($user2);
+
+        $user3 = new User();
+        $user3->setFirstName('sarah');
+        $user3->setLastName('adberemane');
+        $user3->setPlainPassword('sailorMoon');
+        $user3->setDateBirth($faker->dateTime($max = 'now'));
+        $user3->setAddress($faker->address);
+        $user3->setPhoneNumber($faker->phoneNumber);
+        $user3->setDrivingLicense($faker->bothify('##??#####'));
+        $user3->setEmail('sarahabderemane@gmail.com');
+        $manager->persist($user3);
+        $manager->flush();
+
+        $user4 = new User();
+        $user4->setFirstName('pathé');
+        $user4->setLastName('barry');
+        $user4->setPlainPassword('kyller92');
+        $user4->setDateBirth($faker->dateTime($max = 'now'));
+        $user4->setAddress($faker->address);
+        $user4->setPhoneNumber($faker->phoneNumber);
+        $user4->setDrivingLicense($faker->bothify('##??#####'));
+        $user4->setEmail('kyller92@hotmail.fr');
+        $manager->persist($user4);
+        $manager->flush();
+
+        $adm = new User();
+        $adm->setFirstName('admin');
+        $adm->setLastName('admin');
+        $adm->setPlainPassword('admin');
+        $adm->setDateBirth($faker->dateTime($max = 'now'));
+        $adm->setAddress($faker->address);
+        $adm->setPhoneNumber($faker->phoneNumber);
+        $adm->setDrivingLicense($faker->bothify('##??#####'));
+        $adm->setEmail('admin@hotmail.fr');
+        $adm->setRoles(array('ROLE_ADMIN'));
+        $manager->persist($adm);
+        $manager->flush();
+    }
+    
+    public function getOrder()
+    {
+        return 5;
+    }
+    
+}

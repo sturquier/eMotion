697778de05d256f9b6d24cde85e19d14bd811df1
Simon Turquier <turquiersimon@hotmail.fr>
2017-09-25 14:35:00 +0200

    Refactor bundles for annotation controllers

--- src/LocationBundle/Controller/DefaultController.php
+++ src/LocationBundle/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace LocationBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	/**
+     * @Route("/", name="homepage")
+     */
     public function indexAction()
     {
         return $this->render('LocationBundle:Default:index.html.twig');

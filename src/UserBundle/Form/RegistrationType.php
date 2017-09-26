02bc4577ff86d76a78a277752e7f9a74f8d00402
Simon Turquier <turquiersimon@hotmail.fr>
2017-09-26 09:54:07 +0200

    Fix merge bug on register form

--- src/UserBundle/Form/RegistrationType.php
+++ src/UserBundle/Form/RegistrationType.php
@@ -7,27 +7,17 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 
-class RegistrationType extends AbstractType{
+class RegistrationType extends AbstractType
+{
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-<<<<<<< HEAD
         ->add('firstName', TextType::class)
         ->add('lastName', TextType::class)
         ->add('address', TextType::class)
         ->add('dateBirth', DateType::class)
         ->add('phoneNumber', TextType::class)
-        ->add('drivingLicence', TextType::class)
-        
-        ;
-    }
-
-    public function getBlockPrefix()
-    {
-        return 'app_user_registration';
-=======
-        ->add('address', TextType::class);
->>>>>>> 8bd65c67a3c87d0738aa6b3048235d76f2052071
+        ->add('drivingLicence', TextType::class);
     }
 
     public function getParent()

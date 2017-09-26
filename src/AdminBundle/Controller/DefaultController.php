e42d284a8f80181c7e6583390a2e988feb6d84d5
kyller92 <kyller92@hotmail.fr>
2017-09-26 11:58:12 +0200

    delete user from profile page

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -23,6 +24,20 @@ class DefaultController extends Controller
 		return $this->render('AdminBundle:default:admin_view_users.html.twig', [
 			'users' => $users,
 		]);
+	}
+
+	/**
+     * Delete a user entity.
+     *
+     * @Route("/user/{id}/delete", name="admin_delete_user")
+     */
+	public function adminDeleteUserAction(Request $request, User $user)
+	{
+		$em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->redirectToRoute('homepage');
 	}
 
 }

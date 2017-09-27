96d65a03e3a0cf1151bd56421d37d618ef5c9259
StarOfTheMoon <sarahabderemane@gmail.com>
2017-09-27 10:54:37 +0200

    view payment and form associated

--- src/PaymentBundle/Controller/DefaultController.php
+++ src/PaymentBundle/Controller/DefaultController.php
@@ -2,10 +2,16 @@
 
 namespace PaymentBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	/**
+     * Payment 
+     *
+     * @Route("/payment", name="payment_view")
+     */
     public function indexAction()
     {
         return $this->render('PaymentBundle:Default:index.html.twig');

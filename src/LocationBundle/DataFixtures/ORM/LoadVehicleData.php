68af1f5d0d262678ecef2348a8c37b5a7433944a
StitchMoonz <lee-sophie@live.fr>
2017-09-28 11:27:54 +0200

    Modification numberPlate structure

--- src/LocationBundle/DataFixtures/ORM/LoadVehicleData.php
+++ src/LocationBundle/DataFixtures/ORM/LoadVehicleData.php
@@ -23,7 +23,7 @@ class LoadVehicleData extends AbstractFixture implements OrderedFixtureInterface
             $vehicle->setModel($faker->vehicleModel);
             $vehicle->setSerialNumber(strtoupper($faker->bothify('#??#?###??##?##??')));
             $vehicle->setColor($faker->safeColorName);
-            $vehicle->setNumberPlate($faker->vehicleRegistration);
+            $vehicle->setNumberPlate(strtoupper($faker->bothify('??-###-?? ##')));
             $vehicle->setKilometer($faker->numberBetween($min = 1000, $max = 100000));
             $vehicle->setDatePurchase($faker->dateTime($max = 'now', $timezone = date_default_timezone_get()));
             $vehicle->setPricePurchase($faker->randomFloat($nbMaxDemicals = 2, $min = 5000, $max = 1000000));        

a4af8060abb4b86e0aa294a301b78a0a806ec054
StarOfTheMoon <sarahabderemane@gmail.com>
2017-10-02 14:51:17 +0200

    vehicle validation

--- src/LocationBundle/Form/VehicleType.php
+++ src/LocationBundle/Form/VehicleType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class VehicleType extends AbstractType
 {
@@ -19,14 +21,89 @@ class VehicleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('brand', TextType::class, ['label' => 'Marque'])
-        ->add('model', TextType::class, ['label' => 'Modèle'])
-        ->add('serialNumber', IntegerType::class, ['label' => 'N° de série'])
-        ->add('color', TextType::class, ['label' => 'Couleur'])
-        ->add('numberPlate',TextType::class, ['label' => 'N° d\'immatriculation'])
-        ->add('kilometer', IntegerType::class, ['label' => 'Nombre de kilomètres au compteur'])
-        ->add('datePurchase', DateType::class, ['label' => 'Date d\'achat'])
-        ->add('pricePurchase', NumberType::class, ['label' => 'Prix d\'achat'])
+        ->add('brand', TextType::class, [
+            'label' => 'Marque',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new  Assert\Regex(array(
+                    'pattern' => '/\d/',
+                    'match'   => false
+                )),
+                new Assert\Length(array(
+                    'min' => 3, 
+                    'max' => 80
+                ))
+            )
+        ])
+        ->add('model', TextType::class, [
+            'label' => 'Modèle',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+            ),
+            'attr' => [
+                'placeholder' => 'ex: Vectra',
+            ]
+        ])
+        ->add('serialNumber', TextType::class, [
+            'label' => 'N° de série',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                    'min' => 17, 
+                    'max' => 17
+                ))
+            ),
+            'attr' => [
+                'placeholder' => 'ex: 9PL0B630OZ87B66UX',
+            ]
+        ])
+        ->add('color', TextType::class, [
+            'label' => 'Couleur',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+            ),
+            'attr' => [
+                'placeholder' => 'ex: Saumon',
+            ]
+        ])
+        ->add('numberPlate',TextType::class, [
+            'label' => 'N° d\'immatriculation',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+            ),
+            'attr' => [
+                'placeholder' => 'ex: LR-965-SX 01',
+            ]
+        ])
+        ->add('kilometer', IntegerType::class, [
+            'label' => 'Nombre de kilomètres au compteur',
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank(),
+            ),
+            'attr' => [
+                'placeholder' => 'ex: 15000',
+            ]
+        ])
+        ->add('datePurchase', DateType::class, [
+            'label' => 'Date d\'achat',
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Date()
+            )
+        ])
+        ->add('pricePurchase', NumberType::class, [
+            'label' => 'Prix d\'achat',
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Date()
+            )
+        ])
         ->add('submit', SubmitType::class, ['label' => 'Enregister']);
     }
     
